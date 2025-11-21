939e5b7ffb7639d347ad6f4db8971a176403cab2
LuzukoDaki <lzkdaki@gamil.com>
2025-11-21 12:26:59 +0200

    auto

--- POEClaims/Models/Register.cs
+++ POEClaims/Models/Register.cs
@@ -5,7 +5,7 @@ namespace POEClaim.Models
     public class Register
     {
         public string name { get; set; } // Changed from int to string
-        public string surname { get; set; }
+        public string surname { get; set; } //
         public string email { get; set; }
         public string password { get; set; }
     }
